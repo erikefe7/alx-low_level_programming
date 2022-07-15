890d6cf62fd2d65b6700c11b62a19823eb74848b
erikefe7 <erikefeugherughe@gmail.com>
2022-07-15 04:44:19 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
-
-	nelem = 0;
+	size_t count = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if (h_>str == NULL)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
-		nelem++;
+		count++;
 	}
-	return (nelem);
+
+	return (count);
 }
